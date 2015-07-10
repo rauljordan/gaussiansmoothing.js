a22a745506784b1e6fde11212352bd9e9e96522b
Raul Jordan <rauljordan@college.harvard.edu>
2015-07-10 03:34:11 +0200

    added basic kernel methods

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
-module.exports = function (data) {
-  console.log("Hello World")
-};
+var math = require('mathjs');
+
+function kernel (z) {
+  z.map(function (num) {
+    return math.exp(-num*num/2) / math.sqrt(2*math.pi);
+  });
+}
+
+function gaussianRegression (xs, x, h) {
+  return math.divide(math.subtract(xs, x), h);
+}
+
+console.log(gaussianRegression([3.0,1.0], [3.0, 0.8], 1.0))
