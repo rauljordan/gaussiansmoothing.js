e7868db4c405eea5efcab749fba302826e979d76
Raul Jordan <rauljordan@college.harvard.edu>
2015-07-10 16:56:48 +0200

    added kernel mapper function

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 var math = require('mathjs');
 
 function kernel (z) {
-  z.map(function (num) {
+  return z.map(function (num) {
     return math.exp(-num*num/2) / math.sqrt(2*math.pi);
   });
 }
 
-function gaussianRegression (xs, x, h) {
-  return math.divide(math.subtract(xs, x), h);
+function gaussianRegression (xs, x, y, h) {
+  var z = kernel(math.divide(math.subtract(xs, x), h));
+  return math.sum(math.multiply(z, y)) / math.sum(z);
 }
 
-console.log(gaussianRegression([3.0,1.0], [3.0, 0.8], 1.0))
+
+console.log(gaussianRegression([1,2], [0,1], [9, 3], 1))
